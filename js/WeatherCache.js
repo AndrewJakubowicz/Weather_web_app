e7cb3af306e0882dbb78a1fc84af54d8e8682218
SpyR1014 <spyr1014@gmail.com>
2016-05-15 17:06:20 +1000

    locations can now be added

--- js/WeatherCache.js
+++ js/WeatherCache.js
@@ -2,4 +2,116 @@
  * Created by Spyr1014 on 15/05/2016.
  */
 
+var STORAGE_KEY = "weather_obj";
 
+// Code that needs to execute on the load of every page.
+// --------------------------------------------------- //
+
+var locationsList = new LocationsListCache();
+loadLocations();
+
+// --------------------------------------------------- //
+
+Date.prototype.dateString = function () {
+  function pad (value) {
+    return ("0" + value).slice(-2);
+  }
+
+  return this.getFullYear() + "-" +
+    pad(this.getMonth() + 1) + "-" +
+    pad(this.getDate());
+};
+
+// Date format required by forecast.io api.
+// If the time is always represented as midday
+// time zone errors are avoided.
+Date.prototype.apiDateString = function() {
+  return this.dateString() + "T12:00:00";
+};
+
+function Location (nickname, lat, lngtitude, forecast){
+  var name = nickname;
+  var latitude = lat;
+  var longitude = lngtitude;
+  var forecasts = forecast;
+  if (!forecasts) forecasts = {};
+
+  this.getName = function() {  return name;  };
+  this.getLatitude = function() {  return latitude  };
+  this.getLongitude = function() {  return longitude  };
+
+  // Gets the forecast for a specific date.
+  this.getForecast = function(string) {
+    //TODO: Fill in function
+  };
+
+  this.toJSON = function() {
+    return {
+      name: name,
+      latitude: latitude,
+      longitude: longitude,
+      forecasts: forecasts
+    };
+  };
+
+
+  this.initialiseFromPDO = function(data) {
+    name = data.name;
+    latitude = data.latitude;
+    longitude = data.longitude;
+    forecasts = data.forecasts;
+  };
+}
+
+function LocationsListCache(){
+  var locations = [];
+  // TODO: Work out callbacks.
+
+  this.length = function(){
+    return locations.length;
+  };
+
+  this.locationAtIndex = function(index) {
+    return locations[index];
+  };
+
+  this.addLocation = function(latitude, longitude, name){
+    return locations.push(new Location(name, latitude, longitude)) - 1;
+  };
+
+  this.removeLocationAtIndex = function(index) {
+    locations.splice(index, 1);
+  };
+
+  this.toJSON = function() {
+    return {locations: locations};
+  };
+
+  this.initialiseFromPDO = function(locationsListPDO) {
+    var locationData;
+    locations = []; //Locations emptied just in case.
+    for (var i = 0; i < locationsListPDO.locations.length; i++){
+      locationData = locationsListPDO[i];
+      locations.push(new Location().initialiseFromPDO(locationData));
+    }
+  };
+
+
+
+  function indexForLocation(latitude, longitude){
+    for (var i = 0; i < locations.length; i++){
+      if (latitude == locations[i].getLatitude() && longitude == locations[i].getLongitude())
+        return i;
+    }
+    return -1;
+  }
+}
+
+function loadLocations() {
+  var locationsListJSON = localStorage.STORAGE_KEY;
+  if (locationsListJSON) locationsList.initialiseFromPDO(JSON.parse(locationsListJSON));
+}
+
+function saveLocations() {
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(locationsList));
+}
--- js/addLocation.js
+++ js/addLocation.js
@@ -1,3 +1,70 @@
 /**
  * Created by Spyr1014 on 15/05/2016.
  */
+
+var map;
+var geocoder;
+var savedAddress;
+
+// Function called by google maps script api callback.
+function initMap() {
+  var mapDiv = document.getElementById("map");
+  geocoder = new google.maps.Geocoder();
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: {lat: -37, lng: 145},
+    zoom: 15
+  });
+  mapDiv.style.width = "100%";
+  document.getElementById("scriptMap").remove(); // Cleans up the script call
+}
+
+// Geocode the address inputted by the user
+function geocodeAddress(){
+  var addressInput = document.getElementById("addressInput").value;
+
+  geocoder.geocode({'address': addressInput}, function(results, status) {
+    if (status == google.maps.GeocoderStatus.OK){
+      savedAddress = results[0].geometry.location;
+      map.setCenter(savedAddress);
+      var marker = new google.maps.Marker({
+        map: map,
+        position: savedAddress
+      });
+      document.getElementById("error").innerText = "";
+    } else {
+      document.getElementById("error").innerText = "Error finding Address";
+      savedAddress = "";
+    }
+  })
+}
+
+// Adds input event listener to addressInput, allowing visual location as it is typed in.
+document.getElementById("addressInput").addEventListener("input", geocodeAddress);
+
+// Function that runs on page load
+function initPage() {
+  // Create the google map
+  (function() {
+    var mapScript = document.createElement("script");
+    var googleMapsApi = "https://maps.googleapis.com/maps/api/js?key=" + MAPS_API_KEY + "&callback=initMap";
+    mapScript.setAttribute("id", "scriptMap");
+    mapScript.setAttribute("async", "");
+    mapScript.setAttribute("defer", "");
+    mapScript.setAttribute("src", googleMapsApi);
+    document.body.appendChild(mapScript);
+  })();
+}
+
+function validateSubmit() {
+  var name;
+  if (savedAddress) {
+    name = document.getElementById("nameInput").value;
+    if (!name) name = document.getElementById("addressInput").value;
+    locationsList.addLocation(savedAddress.lat(), savedAddress.lng(), name);
+    saveLocations();
+    return true;
+  } else {
+    alert("Error: Can't find Address. Please check your address is correct");
+    return false
+  }
+}
